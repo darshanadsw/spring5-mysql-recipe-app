5bb79dd849029bfcb919d6e5dc1cf726369cf421
darshanadsw <darshanadsw@gmail.com>
2020-06-29 14:12:45 -0400

    Codecov status updated.

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE dw_dev;
 CREATE DATABASE dw_prod;
 
-CREATE USER 'dw_dev_user'@'localhost' INDENTIFIED BY 'dw';
-CREATE USER 'dw_prod_user'@'localhost' INDENTIFIED BY 'dw';
-CREATE USER 'dw_dev_user'@'%' INDENTIFIED BY 'dw';
-CREATE USER 'dw_prod_user'@'%' INDENTIFIED BY 'dw';
+CREATE USER 'dw_dev_user'@'localhost' IDENTIFIED BY 'dw';
+CREATE USER 'dw_prod_user'@'localhost' IDENTIFIED BY 'dw';
+CREATE USER 'dw_dev_user'@'%' IDENTIFIED BY 'dw';
+CREATE USER 'dw_prod_user'@'%' IDENTIFIED BY 'dw';
 
 GRANT SELECT ON dw_dev.* to 'dw_dev_user'@'localhost';
 GRANT INSERT ON dw_dev.* to 'dw_dev_user'@'localhost';
@@ -23,4 +23,6 @@ GRANT SELECT ON dw_prod.* to 'dw_prod_user'@'%';
 GRANT INSERT ON dw_prod.* to 'dw_prod_user'@'%';
 GRANT UPDATE ON dw_prod.* to 'dw_prod_user'@'%';
 GRANT DELETE ON dw_prod.* to 'dw_prod_user'@'%';
+
+FLUSH PRIVILEGES;
 
